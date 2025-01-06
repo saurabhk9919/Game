cd8ed891b968ea4614753d60c2bf5484c892b8f2
Saurabh Kashyap <153047425+saurabhk9919@users.noreply.github.com>
2025-01-06 23:49:15 +0530

    added new

--- app.js
+++ app.js
@@ -1,20 +1,69 @@
-let boxes=document.querySelectorAll('.box');
-let resey=document.querySelector('.reset');
-let turn=document.querySelector('.turn');
+let boxes = document.querySelectorAll('.box');
+let resey = document.querySelector('.reset');
+let turn = document.querySelector('.turn');
 
-let turnCount=True;
-const winCombos=[[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
+let newgamebrn = document.querySelector('#newbttn');
+let Main = document.querySelector('.main');
+let msgcontainer = document.querySelector('.message');
+let msg = document.querySelector('#msg');
 
-boxes.forEach((box)=>{{box.addEventListener('click',()=>{console.log('clicked')
-    if(turnCount){
-        box.innerHTML='0';
-        turnCount=false;
-        else{
-        turn.innerHTML='X';
-        turnCount=true;}
-        box.disabled=true;
-        checkWin();
+let turnCount = true;
+const winCombos = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
 
-})}});
-const checkWin=()=>{
-}
+const reset = () => {
+    turnCount = true;
+    enableBoxes();
+    msgcontainer.classList.add("hide");
+    boxes.forEach(box => box.innerText = '');
+};
+
+boxes.forEach((box) => {
+    box.addEventListener("click", () => {
+        if (box.innerText === "") {
+            if (turnCount) {
+                box.innerHTML = "0";
+                turnCount = false;
+            }
+            else {
+                box.innerHTML = "X";
+                turnCount = true;
+            }
+            box.disabled = true;
+            winnerCheck();
+        }
+    });
+});
+
+const disableBoxes = () => {
+    boxes.forEach(box => box.disabled = true);
+};
+
+const enableBoxes = () => {
+    boxes.forEach(box => {
+        box.disabled = false;
+    });
+};
+
+const showWinner = (winner) => {
+    msg.innerText = `Winner is ${winner}`;
+    msgcontainer.classList.remove("hide");
+};
+
+const winnerCheck = () => {
+    for (let pattern of winCombos) {
+        let pos1 = boxes[pattern[0]].innerText;
+        let pos2 = boxes[pattern[1]].innerText;
+        let pos3 = boxes[pattern[2]].innerText;
+
+        if (pos1 !== "" && pos2 !== "" && pos3 !== "") {
+            if (pos1 === pos2 && pos2 === pos3) {
+                showWinner(pos1);
+                disableBoxes();
+                return;
+            }
+        }
+    }
+};
+
+newgamebrn.addEventListener("click", reset);
+resey.addEventListener("click", reset);
